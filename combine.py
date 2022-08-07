35786cfe1c2ebe4a15b98c0702c06f788c0cead6
Josue Velasquez <josuevel98@gmail.com>
2022-08-07 15:28:08 -0400

    basic stufff done need to add button to merge and add more visual clarity of whts being done

---
+++ combine.py
@@ -0,0 +1,44 @@
+from PyPDF2 import PdfMerger
+import tkinter as tk
+from tkinter import filedialog
+
+def merge(pdfs):
+    merger = PdfMerger()
+    for pdf in pdfs:
+        merger.append(pdf)
+    merger.write("result.pdf")
+    merger.close()
+
+def get_file_path(pdfs):
+    file_path = filedialog.askopenfilename()
+    pdfs.append(file_path)
+    return file_path
+
+def main():
+    
+    #initialize stuff
+    pdfs = []
+    pdflabels = []
+    window = tk.Tk()
+
+    
+    #create content of window
+    b = tk.Button(window,text="Add pdf",command=(lambda:get_file_path(pdfs)))
+    for pdf in pdfs:
+        label = Label(window, textvariable=pdf, relief=RAISED )
+
+    #add content to window
+    for label in pdflabels:
+        label.pack()
+    b.pack()
+
+    #run window
+    window.geometry("500x200")
+    window.mainloop()
+
+    for pdf in pdfs:
+        print(pdf)
+
+
+if __name__ == "__main__":
+    main()
